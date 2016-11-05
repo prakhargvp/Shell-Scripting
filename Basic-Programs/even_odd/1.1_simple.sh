bda98deeccae8f2c2ca0e26ecd6d935b25d06973
prakhargvp <prakhargvp@gmail.com>
2016-11-05 20:14:57 +0530

    Add basic/even_odd/readme

--- Basic-Programs/even_odd/1.1_simple.sh
+++ Basic-Programs/even_odd/1.1_simple.sh
@@ -1,6 +1,7 @@
 ## To find given no is Even/Odd 
 	echo -n "Enter the number : "
-## We can also use printf
+## We can also use 
+# echo -e "Enter the number :\c"
 # printf "Enter the number : "	
 ## Reading String from user or writting by user 
 	read num
---
+++ Basic-Programs/even_odd/readme.txt
@@ -0,0 +1,8 @@
+Shell Commands Used
+1. echo
+2. printf
+
+Shell Script Syntax
+3. `expr .....`
+4. if-else
+5. read
